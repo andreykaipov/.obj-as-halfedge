eccfbc180dce78836fd2023300b20532a48035b0
Andrey Kaipov <akaip002@fiu.edu>
2016-03-11 02:34:17 -0500

    Move good data verification test to objparser

--- OBJParser/OBJParser.rb
+++ OBJParser/OBJParser.rb
@@ -8,15 +8,45 @@ class OBJParser
         mesh = Mesh.new([], [])
 
         File.open(fileName, "r") do |file|
+
+            # The lines we're interested in look like "v x y z", or like "f v1 v2 v3 ..."
             file.each_line do |line|
-                # A line looks like "v x y z", or like "f v1 v2 v3 ..."
+
                 if line[0] == 'v' && line[1] == ' ' then
-                    mesh.vertices << line.slice(2..-1).split(' ').map(&:to_f)
+                    mesh.vertices << line.split(' ').drop(1).map(&:to_f)
+
                 elsif line[0] == 'f' && line[1] == ' ' then
                     # Vertices are 1-indexed in obj files. We like 0-indexed.
-                    mesh.faces << line.slice(2..-1).split(' ').map(&:to_i).map(&:pred)
+                    face = line.split(' ').drop(1).map(&:to_i).map(&:pred)
+
+                    face.each_with_index do |_, i|
+                        if face[i - 1] == face[i] then
+                            abort "Woah! That's a bad obj file.\n"\
+                            "There is a half-edge whose source and target vertex are the same!\n"\
+                            "Offending half-edge occurs in the face `f #{face.map{|v| v+1}.join(' ')}`.\n"\
+                            "Vertex in question is `v #{mesh.vertices[face[i]][0]}" "#{mesh.vertices[face[i]][1]} #{mesh.vertices[face[i]][2]}`."
+                        end
+                    end
+
+                    mesh.faces << face
+
                 end
+
             end
+
+        end
+
+        # Before returning, find duplicate vertices with their sizes in linear time!
+        fakeVertices = mesh.vertices.group_by{|v| v}.select{|k,v| v.size > 1}.map{|k,v| [k, v.size]}
+        unless fakeVertices.empty?
+            puts "\nYou have several pseudo-unique vertices in this mesh. Say what?\n"\
+                 "The following vertices are listed uniquely in your .obj file,\n"\
+                 "but have identical coordinates:\n\n"
+            fakeVertices.each do |fv, multiplicity|
+                puts "(#{fv[0]}, #{fv[1]}, #{fv[2]}) occurs #{multiplicity} times."
+            end
+            puts "\nThis is fine, but the total curvature of the surface may be a bit off"
+            puts "since we'll ignore the angles formed by adjacent edges on these vertices."
         end
 
         return mesh
